c0fdb251aeb8a0c67ecb73e5c513d4a5aded0fa3
dtduc <ducdt@vntechno.vn>
2021-06-22 19:47:22 +0700

    Add perf test

--- index.test.js
+++ index.test.js
@@ -1,20 +1,35 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
+
 const SITE_URL = "https://k04-testing-sm.dtduc.name.vn/"
 const LOGIN_URL = `${SITE_URL}Login.aspx`
 const PRODUCT_URL = `${SITE_URL}Products.aspx`
+const HOMEPAGE_URL = `${SITE_URL}Default.aspx`
 
-const BROWSER = 'safari'
+const BROWSER = 'chrome'
+
+const screen = {
+	width: 1920,
+	height: 1080
+  };
 const getDriver = async () => {
-	return new Builder().forBrowser(BROWSER).build();
+	let builder = new Builder().forBrowser(BROWSER);
+	if (BROWSER === 'chrome') {
+		builder = builder
+			.setChromeOptions(new chrome.Options().headless().windowSize(screen))
+			.setFirefoxOptions(new firefox.Options().headless().windowSize(screen))
+	}
+	return builder.build();
 }
 describe(SITE_URL, () => {
 	let driver;
-	beforeEach(async () => {
+	beforeAll(async () => {
 		driver = await getDriver();
 	});
 
-	afterEach(async () => {
-		driver.quit();
+	afterAll(async () => {
+		await driver.quit();
 	});
 
 	it("Login successfully", async () => {
@@ -42,8 +57,26 @@ describe(SITE_URL, () => {
 		let itemCountInCart = await itemCountInCartEl.getText();
 		expect(parseInt(itemCountInCart, 10)).toEqual(1);
 	});
+});
+describe("Performance test", () => {
+	beforeEach(async () => {
+		driver = await getDriver();
+	});
 
+	afterEach(async () => {
+		await driver.quit();
+	});
 
-
+	jest.setTimeout(100 * 1000) // 100s
+	it('100 CCU on homepage within 100s: ', async () => {
+		console.time('test');
+		const reqArr = []
+		for (let index = 0; index < 100; index++) {
+			reqArr.push(driver.get(HOMEPAGE_URL));
+		}
+		await Promise.all(reqArr)
+		console.time('test');
+		// expect(parseInt(itemCountInCart, 10)).toEqual(1);
+	});
 });
 //==================== End Main ====================
